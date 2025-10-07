85e9b6e717fe1287e9c8f4ed78fc517f9c3fff67
Luis Fernando Flores <a01738347@tec.mx>
2025-10-07 13:14:48 -0600

    Added Plot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,33 @@ import { useRef, useState } from 'react'
 // import './App.css'
 import '@aws-amplify/ui-react/styles.css';
 import { Button, SliderField } from "@aws-amplify/ui-react";
+import Plot from 'react-plotly.js';
 
 function App() {
   let [location, setLocation] = useState("");
   let [trees, setTrees] = useState([]);
   let [gridSize, setGridSize] = useState(20);
   let [simSpeed, setSimSpeed] = useState(1);
-  let [density, setDensity] = useState(0.4);
+  let [density, setDensity] = useState(0.4); // Nuevo estado para densidad
   const running = useRef(null);
+  const simTime = useRef([]);
+  const burntPerc = useRef([]);
+  const vizdata = useRef(
+          {
+            x: [],
+            y: [],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+        );
 
   let setup = () => {
     console.log("Hola");
     fetch("http://localhost:8000/simulations", {
       method: 'POST',
+      body: JSON.stringify({ dim: [gridSize, gridSize], density: density }), // Enviar densidad
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ dim: [gridSize, gridSize], density: density })
     }).then(resp => resp.json())
     .then(data => {
       console.log(data);
@@ -31,47 +43,64 @@ function App() {
       fetch("http://localhost:8000" + location)
       .then(res => res.json())
       .then(data => {
+        const trees = data["trees"];
+        var perc = trees.filter((tree) => tree.status === "burnt").length / trees.length;
+        simTime.current = [...simTime.current, simTime.current.length + 1];
+        burntPerc.current = [...burntPerc.current, perc];
         setTrees(data["trees"]);
       });
     }, 1000 / simSpeed);
   };
 
   const handleStop = () => {
+    vizdata.current = {...vizdata.current, x: simTime.current, y: burntPerc.current};
+    console.log(vizdata.current);
     clearInterval(running.current);
+    console.log(simTime.current, burntPerc.current);
   }
-
 
   let burning = trees.filter(t => t.status == "burning").length;
 
   if (burning == 0)
     handleStop();
 
-  let offset = 50;
+  let offset = 10; // (500 - gridSize * 12) / 2;
   return (
     <>
       <div>
-
         <Button 
           variation="primary"
-          colorTheme="overlay"
+          colorTheme="overlay" 
           onClick={setup}>
           Setup
         </Button>
-        <Button 
-        variation="primary"
-        colorTheme="overlay"
-        onClick={handleStart}>
+        <Button variant={"contained"} onClick={handleStart}>
           Start
         </Button>
-        <Button 
-        variation="primary"
-        colorTheme="overlay"
-        onClick={handleStop}>
+        <Button variant={"contained"} onClick={handleStop}>
           Stop
         </Button>
-        <SliderField label="Grid size" min={10} max={40} step={10} type='number' value={gridSize} onChange={setGridSize}/>
-        <SliderField label="Simulation speed" min={1} max={10} step={1} type='number' value={simSpeed} onChange={setSimSpeed}/>
-        <SliderField label="Density" min={0.1} max={1.0} step={0.1} type='number' value={density} onChange={setDensity}/>
+        <SliderField 
+          label="Grid size" 
+          min={10} max={40} step={10} 
+          type='number' 
+          value={gridSize} 
+          onChange={setGridSize}/>
+
+        <SliderField 
+          label="Simulation speed" 
+          min={1} max={10} 
+          type='number'
+          value={simSpeed} 
+          onChange={setSimSpeed}/>
+
+        <SliderField // Nuevo slider de densidad
+          label="Density"
+          min={0.1} max={1.0} step={0.1}
+          type='number'
+          value={density}
+          onChange={setDensity}
+        />
       </div>
       <svg width="500" height="500" xmlns="http://www.w3.org/2000/svg" style={{backgroundColor:"white"}}>
       {
@@ -89,6 +118,10 @@ function App() {
         )
       }
       </svg>
+      <Plot
+        data={[vizdata.current]}
+        layout={ {width: 320, height: 240, title: {text: 'A Fancy Plot'}} }
+      />
     </>
   );
 }
